d5872b37d3095adb28e6d6cae6e7d23a688fdd07
zacck <osiemobiz@gmail.com>
2014-09-23 18:40:39 +0300

    we have separated the checking class for google play services just clean up no functionality

--- src/com/zacck/locationwhisperer/MainActivity.java
+++ src/com/zacck/locationwhisperer/MainActivity.java
@@ -121,38 +121,7 @@ public class MainActivity extends ActionBarActivity implements
 		}
 	}
 
-	// make a var to check if we good
-	private boolean servicesConnected() {
-		// Check that Google Play services is available
-		int resultCode = GooglePlayServicesUtil
-				.isGooglePlayServicesAvailable(this);
-		// If Google Play services is available
-		if (ConnectionResult.SUCCESS == resultCode) {
-			// In debug mode, log the status
-			Log.d("Location Updates", "Google Play services is available.");
-			// Continue
-			return true;
-			// Google Play services was not available for some reason.
-			// resultCode holds the error code.
-		} else {
-			// Get the error dialog from Google Play services
-			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-					resultCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
 
-			// If Google Play services can provide an error dialog
-			if (errorDialog != null) {
-				// Create a new DialogFragment for the error dialog
-				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-				// Set the dialog in the DialogFragment
-				errorFragment.setDialog(errorDialog);
-				// Show the error dialog in the DialogFragment
-				errorFragment.show(getSupportFragmentManager(),
-						"Location Updates");
-			}
-			return false;
-		}
-
-	}
 
 	@SuppressLint("NewApi")
 	@Override
